2f5995520de59472e59d2efbc2315602db11e032
t3rmian <t3rmian@gmail.com>
2019-08-09 20:19:13 +0200

    Up the CI config

--- build.sh
+++ build.sh
@@ -7,19 +7,28 @@ then
     curl -sL https://install-node.now.sh | bash -s --
 fi
 
-npm init -y
-npm install html-minifier
-npm install cssnano
-npm install cssnano-cli
-npm install uglify-js
+sudo npm install -g html-minifier
+sudo npm install -g cssnano
+sudo npm install -g cssnano-cli
+sudo npm install -g uglify-js
 rm -rf dist/ 
 mkdir dist dist/css dist/img
 
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true --minify-js true --input-dir ./ --output-dir dist/ --file-ext html
-cssnano css/main.css > dist/css/main.css
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true --minify-js true --input-dir ./ --output-dir dist/ --file-ext html || exit 2
+cssnano css/main.css > dist/css/main.css || exit 3
 cp img/* dist/img
 
 export GIT_DEPLOY_DIR=dist
 export GIT_DEPLOY_BRANCH=master
 
-curl -sL https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh | bash -s --
+curl -L https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh > deploy.sh
+MD5=($(md5sum deploy.sh))
+originalMD5=${git_directory_deploy_MD5:-ce930643d4b40f9afc54b91a890d9802}
+if [[ "$MD5" == "$originalMD5" ]]
+then
+    sudo chmod +x deploy.sh
+    bash deploy.sh || exit 4
+else
+    echo "Error: The MD5 hash for the deployment script did not match!"
+    exit 1
+fi
